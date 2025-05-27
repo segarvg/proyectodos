e08f11a7134eff83751a271e5bb95e2d1e7130f8
Anselmo Ortega <lfkanselmo@gmail.com>
2025-05-27 11:08:53 -0500

    feat datos creados

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+      String nombre1= "sebastian garzon villada";
+      String nombre2= "ana sofia muñoz sosa";
+        String nombre3= "sebastian chavez";
+        String nombre4= "alejandra carmona";
+        String nombre5= "tatiana poddubnyak";
+        String nombre6= "jacobo soto rojas";
+        int edad1=23
+        int edad2=18
+        int edad3=21
+        int edad4=18
+        int edad5=35
+        int edad6=19
+        int cedula1=1000654102
+        int cedula2=1018236586
+        int cedula3=1000190561
+        int cedula4=1033489179
+        int cedula5=453590
+        int cedula6=1017926485
+
+    }
+}
